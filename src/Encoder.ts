57fa82537151786a664f759cbff4dcdccef66596
haxecoder <haxecoder@gmail.com>
2024-05-09 22:46:33 +0300

    add encode implementation

--- src/Encoder.ts
+++ src/Encoder.ts
@@ -6,6 +6,36 @@ export class Encoder {
         private readonly tableInt: string,
     ) {}
     public encode(input: number[]): string {
-        return "";
+        const chars: string[] = [];
+
+        input.forEach(value => {
+            if (value.toString().length === 1) {
+                chars.push(this.table1.charAt(value));
+                return;
+            }
+
+            if (value % this.table23.length === 0) {
+                chars.push(this.tableInt.charAt(value / this.table23.length));
+                return;
+            }
+
+            const int = Math.floor(value / this.table23.length);
+
+            if (int === 0) {
+                chars.push(this.table23.charAt(value));
+                return;
+            }
+
+            chars.push(`${int}`);
+            if (value % this.table23.length === 0) {
+                return;
+            }
+
+            const remain = value % this.table23.length;
+
+            chars.push(this.table23.charAt(remain));
+        });
+
+        return chars.join("");
     }
 }
